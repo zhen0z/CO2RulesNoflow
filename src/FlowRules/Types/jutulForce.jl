a206bfefae910c3ad937c1dd6a1abdb8634a8911
zhen0z <zhen.zhang.cc@gmail.com>
2024-09-20 23:26:03 +0100

    Update jutulForce.jl

--- src/FlowRules/Types/jutulForce.jl
+++ src/FlowRules/Types/jutulForce.jl
@@ -1,19 +1,3 @@
-# export jutulForce
-
-# struct jutulForce{D, T}
-#     irate::T
-#     name::Vector{Symbol}
-#     loc::Vector{NTuple{D, T}}
-# end
-
-# jutulForce(irate::T, loc::NTuple{D, T}) where {D, T} = jutulForce(irate, [loc])
-# jutulForce(irate::T, loc::Vector{NTuple{D, T}}) where {D, T}= jutulForce(irate, vcat(:Injector, [:Producer for i = 1:length(loc)]), loc)
-
-# display(f::jutulForce{D, T}) where {D, T} =
-#     println("$(D)D jutulForce structure with $(length(f.loc)) injection/production wells and rate $(f.irate) m^3/s")
-
-# ==(A::jutulForce{D, T}, B::jutulForce{D, T}) where {D,T} = (A.irate == B.irate && A.name == B.name && A.loc == B.loc)
-
 export jutulForce
 
 struct jutulForce{D, T}
@@ -23,9 +7,25 @@ struct jutulForce{D, T}
 end
 
 jutulForce(irate::T, loc::NTuple{D, T}) where {D, T} = jutulForce(irate, [loc])
-jutulForce(irate::T, loc::Vector{NTuple{D, T}}) where {D, T}= jutulForce(irate, vcat(:Injector, [:Injector for i = 1:length(loc)]), loc)
+jutulForce(irate::T, loc::Vector{NTuple{D, T}}) where {D, T}= jutulForce(irate, vcat(:Injector, [:Producer for i = 1:length(loc)]), loc)
 
 display(f::jutulForce{D, T}) where {D, T} =
     println("$(D)D jutulForce structure with $(length(f.loc)) injection/production wells and rate $(f.irate) m^3/s")
 
 ==(A::jutulForce{D, T}, B::jutulForce{D, T}) where {D,T} = (A.irate == B.irate && A.name == B.name && A.loc == B.loc)
+
+# export jutulForce
+
+# struct jutulForce{D, T}
+#     irate::T
+#     name::Vector{Symbol}
+#     loc::Vector{NTuple{D, T}}
+# end
+
+# jutulForce(irate::T, loc::NTuple{D, T}) where {D, T} = jutulForce(irate, [loc])
+# jutulForce(irate::T, loc::Vector{NTuple{D, T}}) where {D, T}= jutulForce(irate, vcat(:Injector, [:Injector for i = 1:length(loc)]), loc)
+
+# display(f::jutulForce{D, T}) where {D, T} =
+#     println("$(D)D jutulForce structure with $(length(f.loc)) injection/production wells and rate $(f.irate) m^3/s")
+
+# ==(A::jutulForce{D, T}, B::jutulForce{D, T}) where {D,T} = (A.irate == B.irate && A.name == B.name && A.loc == B.loc)
